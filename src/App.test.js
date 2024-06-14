ada0103a96d730c5002b860aa989a62105b7d5e3
VSP1000 <surya@criodo.com>
2024-06-14 12:04:09 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,54 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+/* eslint-disable no-undef */
+test("correctly calculates the percentage of floating point numbers", () => {
+    const mockSetCalc = jest.fn();
+    const mockCalc = {
+        sign: "",
+        num: "5.4",
+        res: "0",
+    };
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+    percentClickHandler(mockSetCalc, mockCalc);
+
+    expect(mockSetCalc).toHaveBeenCalledWith({
+        ...mockCalc,
+        num: "0.054",
+        res: "0",
+        sign: "",
+    });
+});
+
+test("correctly calculates the percentage of zero", () => {
+    const mockSetCalc = jest.fn();
+    const mockCalc = {
+        sign: "",
+        num: "0",
+        res: "100",
+    };
+
+    percentClickHandler(mockSetCalc, mockCalc);
+
+    expect(mockSetCalc).toHaveBeenCalledWith({
+        ...mockCalc,
+        num: "0",
+        res: "1",
+        sign: "",
+    });
+});
+
+test("keeps the result as zero when there is no number defined", () => {
+    const mockSetCalc = jest.fn();
+    const mockCalc = {
+        sign: "+",
+        num: "",
+        res: "10",
+    };
+
+    percentClickHandler(mockSetCalc, mockCalc);
+
+    expect(mockSetCalc).toHaveBeenCalledWith({
+        ...mockCalc,
+        num: "0",
+        res: "10",
+        sign: "",
+    });
 });
